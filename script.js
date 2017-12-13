f19d286431ca5cf07ce28e24712d39f75a9ee3b2
mutasem <mutasem.halawani@gmail.com>
2017-12-13 20:28:22 +0200

    Use material design for Styling and fix ajax call for wiki api

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// let searchTerm = 'Jerusalem'; 
 
-$('i.fa-search').on('click',function(){
-  $('input').css("display", "block");
-  $(this).hide().animate();
+$('button').on('click',function(){
+
+	let query = $('#searchForm').val();
+	searchWiki(query);
+	console.log(query);
+	// console.log(this.value);
 })
+
+
+let searchWiki = function (searchTerm){
+	$.ajax({
+		url: `https://en.wikipedia.org/w/api.php?action=query
+		&format=json&list=search&continue=-%7C%7C
+		&srsearch=${searchTerm}&srlimit=10&sroffset=10`,
+		dataType: 'jsonp',
+		success: function(data) {
+			console.log(data.query.search[0].title);
+			console.log(data.query.search[0].pageid);
+			console.log(data.query.search[0].snippet);
+
+			let resultTitle = data.query.search[0].title;
+			let resultSnippet = data.query.search[0].snippet;
+			let pageId = data.query.search[0].pageid;
+			let resultLink = `https://en.wikipedia.org/w/api.php?action=query
+			&prop=info&pageids=${pageId}&inprop=url`
+			
+			let html = `
+			<div class="cards-section">
+				<div class="card">
+					<a href="${resultLink}">
+						<div class="card-body">
+							<h4 class="card-title">${resultTitle}</h4>
+							<p class="card-text">${resultSnippet}</p>
+						</div>
+					</a>
+				</div>
+			</div>
+			`
+			$('.search-section').append(html);
+		}
+	})
+}
